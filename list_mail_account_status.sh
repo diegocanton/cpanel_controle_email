76b12a6eab03b61951aa957edca097a7264fe6ee
Diego Canton de Brito <diegocantonbgi@gmail.com>
2018-04-09 16:07:12 -0300

    Create list_mail_account_status.sh

---
+++ list_mail_account_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+uapi --user=<USER_CPANEL> Email list_pops | sed "s/\n//g" > list_contas
+echo "" > delete_log
+
+for mailaccount in `cat /opt/limpaconta/list_contas | sed "s/\s//g"`; do
+    if [[ $mailaccount =~ ^login: ]]; then
+	suspenso=`echo "$mailaccount" | sed "s/login://g"`
+    fi
+    if [[ $mailaccount =~ ^suspended_incoming:1 ]]; then
+	echo "$suspenso suspensa -> por estar: $mailaccount" >> delete_log
+	echo "uapi --user=<USER_CPANEL> Email delete_pop email=$suspenso"
+    fi
+done
